44db6e17e48a5df78950b78443a02c9a8a75441a
Benjamin Cooper <bencooper222@gmail.com>
2020-05-13 06:06:26 +0000

    add the env syncing script and the appropriate types for it since I'm not rewriting it in js

---
+++ scripts/pushEnv.ts
@@ -0,0 +1,79 @@
+// from https://github.com/bencooper222/build-your-own-mint
+import { config as dotenvConfig, parse as dotenvParse } from 'dotenv';
+import { readFileSync } from 'fs';
+import { resolve as pathResolve } from 'path';
+import fetch from 'node-fetch';
+
+dotenvConfig();
+const { GITHUB_USERNAME, GITHUB_PROJECT, CIRCLE_CI_TOKEN } = process.env;
+if (!GITHUB_USERNAME) throw new Error('No GITHUB_USERNAME available!');
+if (!GITHUB_PROJECT) throw new Error('No GITHUB_PROJECT available!');
+if (!CIRCLE_CI_TOKEN) throw new Error('No CIRCLE_CI_TOKEN available!');
+
+// variables that shouldn't be synced
+const filteredVariables = ['CIRCLE_CI_TOKEN', 'GITHUB_USERNAME', 'GITHUB_PROJECT'];
+
+interface NameValPair {
+  name: string;
+  value: string;
+}
+
+const deleteCurrentEnvironmentVariables = async () => {
+  const envVars = await (
+    await fetch(
+      `https://circleci.com/api/v1.1/project/github/${GITHUB_USERNAME}/${GITHUB_PROJECT}/envvar?circle-token=${CIRCLE_CI_TOKEN}`,
+      { method: 'GET' },
+    )
+  ).json();
+
+  for (const { name, value } of envVars) {
+    try {
+      await fetch(
+        `https://circleci.com/api/v1.1/project/github/${GITHUB_USERNAME}/${GITHUB_PROJECT}/envvar/${name}?circle-token=${CIRCLE_CI_TOKEN}`,
+        { method: 'DELETE' },
+      );
+      console.log(`Successfully deleted ${name}: ${value}`);
+    } catch (err) {
+      console.log(`Failed to delete ${name}: ${value}`);
+    }
+  }
+
+  console.log('Finished deleting env vars');
+};
+
+const updateSingleToken = async (nameValToken: NameValPair) => {
+  console.log(
+    'Updated this pair: ' +
+      JSON.stringify(
+        await fetch(
+          `https://circleci.com/api/v1.1/project/github/${GITHUB_USERNAME}/${GITHUB_PROJECT}/envvar?circle-token=${CIRCLE_CI_TOKEN}`,
+          {
+            body: JSON.stringify(nameValToken),
+            headers: { 'Content-Type': 'application/json' },
+            method: 'post',
+          },
+        ).then(res => res.json()),
+      ),
+  );
+};
+
+const syncAllVariables = async () => {
+  await deleteCurrentEnvironmentVariables(); // clear out current env vars
+
+  const envPath = pathResolve(__dirname, '../.env');
+  const envVars = dotenvParse(readFileSync(envPath));
+
+  // don't sync certain variables
+  for (const key in envVars) if (filteredVariables.includes(key)) delete envVars[key];
+
+  // converts to an array of name-value pairs that we can send to circleci
+  const arrayOfNameValPairs: NameValPair[] = Object.entries(
+    envVars,
+  ).map(([name, value]) => ({ name, value: value as string }));
+
+  for (const pair of arrayOfNameValPairs) {
+    await updateSingleToken(pair);
+  }
+};
+
+syncAllVariables();
