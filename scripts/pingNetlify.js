cd587c3274cfaefb8b28715a5e0572333d48bac6
Benjamin Cooper <bencooper222@gmail.com>
2020-07-15 04:01:21 +0000

    correct script call and update script

--- scripts/pingNetlify.js
+++ scripts/pingNetlify.js
@@ -1,7 +1,17 @@
 const fetch = require('node-fetch');
 
 if (process.env.CI !== 'true') require('dotenv').config();
+(async () => {
+  try {
+    const res = await fetch(process.env.NETLIFY_BUILD_HOOK, {
+      method: 'POST',
+    });
+    if (res.ok) {
+      console.log('Pinged netlify!');
+      process.exit(0);
+    }
+  } catch (err) {}
 
-fetch(process.env.NETLIFY_BUILD_HOOK, {
-  method: 'POST',
-});
+  console.error("Ping didn't work");
+  process.exit(1);
+})();
